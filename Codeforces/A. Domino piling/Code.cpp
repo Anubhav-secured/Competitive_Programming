3d7d7294cc47dada65fd03c54df8cb8c2fb023d7
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-04-22 23:34:27 +0530

    Add files via upload

---
+++ Codeforces/A. Domino piling/Code.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<climits>
+#include<vector>
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
+#define int long long int
+#define mod 1000000007
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpi vector<pair<int,int>>
+#define mp make_pair
+#define F first
+#define S second
+using namespace std;
+
+signed main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int m,n;
+    cin>>m>>n;
+    cout<<m*n/2<<endl;
+    
+    return(0);
+}
