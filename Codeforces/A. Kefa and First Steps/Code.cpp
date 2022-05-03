bf51f7f2a564d7daf0013eca517c7a1bbd7f3e10
Anubhav singh <85066847+Anubhav-secured@users.noreply.github.com>
2022-05-03 09:46:35 +0530

    Add files via upload

---
+++ Codeforces/A. Kefa and First Steps/Code.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define mod 1000000007
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpi vector<pair<int,int>>
+#define mp make_pair
+const int N = 1e5+10;
+using namespace std;
+
+signed main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int n, l(1), ml(1);
+    cin >> n;
+    int profits[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> profits[i];
+        if (i > 0)
+            if (profits[i] >= profits[i - 1])
+            {
+                l++;
+                ml = max(ml, l);
+            }
+            else
+                l = 1;
+    }
+
+    cout << ml << endl;
+    return 0;
+}
